81f1de74080550aec5bcce704f1312739dfb5d42
Matheus Sampaio <54461819+Matheus-Sampaio@users.noreply.github.com>
2021-09-23 16:18:19 -0300

    First Commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+// Navigation
+import DrawerNavigation from './src/navigation/DrawerNavigation'
+
+class App extends Component{
+
+  render(){
+    return(
+      <DrawerNavigation />
+    );
+  }
+}
+
+export default App;
